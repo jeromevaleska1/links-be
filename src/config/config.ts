09d61261cdbe0e4c4ca48789599e13a631eacaee
Alex <alexx.public@gmail.com>
2021-11-28 20:23:26 +0100

    ssl added to heroku

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,4 +1,4 @@
-import { Client } from 'pg';
+import { Client, ClientConfig } from 'pg';
 
 interface IAppConfig{
     pgClient: Client,
@@ -15,13 +15,17 @@ CREATE  TABLE if not exists urls(
 `
 
 export const config = async ():Promise<IAppConfig> => {
-    const options = {
+    const options:ClientConfig = {
       host: process.env.PGHOST ?? 'localhost',
       port: +process.env.PGPORT ?? 5432,
       password: process.env.PGPASSWORD ?? 'password',
       user: process.env.PGUSER ?? 'postgres',
   }
-  process.env.PGDATABASE && (options['database'] = process.env.PGDATABASE)
+  // for heroku deployment
+  if(process.env.PGDATABASE) {
+    options['database'] = process.env.PGDATABASE
+    options['ssl'] = true
+  } 
     console.log(process.env)
     const client = new Client(options)
     await client.connect()
