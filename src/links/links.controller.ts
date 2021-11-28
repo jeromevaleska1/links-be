a951a34d2b1ef6de3dd00a47f1595fcfd34fe9d9
Alex <alexx.public@gmail.com>
2021-11-29 00:05:57 +0100

    swagger def upd

--- src/links/links.controller.ts
+++ src/links/links.controller.ts
@@ -1,12 +1,14 @@
 import { Body, Controller, Get, Post, Req, Session } from '@nestjs/common';
 import { LinksService } from './links.service';
 import { Request } from 'express';
+import { ApiBody, ApiProperty } from '@nestjs/swagger';
 
 interface RequestWithSession extends Request{
     sessionID: string
 }
 
 export class GetLinkDTO{
+    @ApiProperty({required: true})
     url: string;
 }
 
@@ -15,22 +17,19 @@ export class LinksController {
     constructor(private linksService: LinksService ){}
     
     @Post('link')
+    @ApiBody({ type: GetLinkDTO })
+
     async getLink(@Body() body:GetLinkDTO, @Req() req: RequestWithSession, @Session() session: { views?: number }){
         session.views = (session.views || 0) + 1;
-        // console.log(req)
         const { sessionID } = req; 
         const link = await this.linksService.getLink({sessionId: sessionID, views:session.views, longUrl:body.url})
-        console.log({sessionID, link})
         return {link , views: session.views}
     }
 
     @Get('user')
     async getUserData(@Req() req: RequestWithSession){
         const { sessionID } = req; 
-        console.log({sessionID})
-        const res = await this.linksService.getUserData(sessionID)
-        console.log(res)
-        return res
+        return await this.linksService.getUserData(sessionID)
     }
 
 }
